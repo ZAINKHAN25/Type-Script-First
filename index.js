05e93c99ff64905db38e6723a1c195bccea701a3
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-28 14:56:04 +0500

    Type script Basic

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var message = 'Hello, I am Back';
-var heading = document.createElement('h1');
-heading.textContent = message;
-document.body.appendChild(heading);
+var input1 = document.querySelector('#input1');
+var input2 = document.querySelector('#input2');
+var btn = document.querySelector('#btn');
+function add(num1, num2) {
+    return num1 + num2;
+}
+btn.addEventListener('click', function () {
+    console.log(add(+input1.value, +input2.value));
+});
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-let message: string = 'Hello, I am Back';
+const input1 = document.querySelector('#input1')! as HTMLInputElement;
+const input2 = document.querySelector('#input2')! as HTMLInputElement;
+const btn = document.querySelector('#btn')! as HTMLInputElement;
 
-let heading = document.createElement('h1');
-heading.textContent = message;
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
 
-document.body.appendChild(heading);
+btn.addEventListener('click', () => {
+    console.log(add(+input1.value, +input2.value));
+});
